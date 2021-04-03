78085e42f952f30ad06dcd17e6b07c23f089b276
olesigilai <ole_sigilai@yahoo.com>
2021-04-04 01:27:39 +0300

    all installed apps are registered in the settings and imports os for the functioning of the project

--- Awards/settings.py
+++ Awards/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
-    'bootstrap3',
+    'bootstrap4',
     'django_registration',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'star_ratings'
 ]
 
 MIDDLEWARE = [
@@ -121,5 +125,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR,"static"),
+]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
 
 LOGIN_REDIRECT = '/'
+LOGOUT_REDIRECT_URL = 'login'
+
