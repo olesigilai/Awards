fa7fcb18e2d993eec73b488443a69375f98c919b
olesigilai <ole_sigilai@yahoo.com>
2021-04-04 01:31:12 +0300

    profile class is added to the model that creates tables for user,bio and photo

--- user/models.py
+++ user/models.py
@@ -1,3 +1,70 @@
 from django.db import models
-
+import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django_countries.fields import CountryField
+from cloudinary.models import CloudinaryField
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    photo = CloudinaryField('profile_pics/', blank=True)
+
+#     def save_profile(self):
+#         self.save()                   
+
+#     def delete_profile(self):
+#         self.delete()
+    
+#     def __str__(self):
+#         return self.bio
+    
+#     class Meta:
+#         verbose_name = 'Profile'
+#         verbose_name_plural = 'Profiles'  
+        
+# class Projects(models.Model):
+#     project_title = models.CharField(max_length=255)
+#     project_image = CloudinaryField('images')
+#     project_description = models.TextField()
+#     pub_date = models.DateTimeField(auto_now_add=True)
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     # author_profile = models.ForeignKey(Profile,on_delete=models.CASCADE, blank=True, default='1')
+#     link = models.URLField()
+#     country = CountryField(blank_label='(select country)', default='Kenya')
+        
+#     def save_project(self):
+#         self.save()
+    
+#     def delete_project(self):
+#         self.delete()
+        
+#     @classmethod
+#     def get_projects(cls):
+#         projects = cls.objects.all()
+#         return projects
+    
+#     @classmethod
+#     def search_projects(cls, search_term):
+#         projects = cls.objects.filter(project_title__icontains=search_term)
+#         return projects
+    
+    
+#     @classmethod
+#     def get_by_author(cls, author):
+#         projects = cls.objects.filter(author=author)
+#         return projects
+    
+    
+#     @classmethod
+#     def get_project(request, id):
+#         try:
+#             project = Projects.objects.get(pk = id)
+            
+#         except ObjectDoesNotExist:
+#             raise Http404()
+        
+#         return project
+    
+#     def __str__(self):
+#         return self.project_title
